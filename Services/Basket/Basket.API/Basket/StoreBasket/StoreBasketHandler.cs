369fa93f1c0baf69b891e48987ddf2cb717a4f2e
ResadCoder <resadsadiqov62@gamil.com>
2025-10-28 22:27:14 +0400

    added grpc talking

--- Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,5 +1,7 @@
 
 
+using Discount.gRPC;
+
 namespace Basket.API.Basket.StoreBasket;
 
 public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
@@ -17,13 +19,30 @@ public class StoreBasketCommandHandlerValidator : AbstractValidator<StoreBasketC
     }
 }
 
-public class StoreBasketHandler(IBasketRepository basketRepository) 
+public class StoreBasketHandler(
+    IBasketRepository basketRepository,
+    DiscountProtoService.DiscountProtoServiceClient discountProtoService
+    ) 
     : ICommandHandler<StoreBasketCommand, StoreBasketResult>
 {
     public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
     {
+        await CalculateDiscount(command.Cart,cancellationToken);
+        
         await basketRepository.StoreBasketAsync(command.Cart, cancellationToken);
 
         return new StoreBasketResult(command.Cart.UserName);
     }
+
+    private async Task CalculateDiscount(ShoppingCart cart, CancellationToken cancellationToken)
+    {
+        foreach (var item in cart.Items)
+        {
+            var coupon = await discountProtoService.GetDiscountAsync(new GetDiscountRequest
+                { ProductName = item.ProductName }, cancellationToken: cancellationToken);
+            
+            item.Price -= coupon.Amount;
+        }
+    }
 }
+
